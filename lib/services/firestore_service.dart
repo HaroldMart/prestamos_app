416841bf1dce191b3fda9c115e1a0d43ae93d682
Haroldy Martinez <HaroldM09@outlook.com>
2023-10-05 16:48:35 -0400

    agregaron metodos de payment

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -112,7 +112,8 @@ Future<void> addLoan(clientId) async {
 
 // Trae datos de un prestamo
 Future<String> getLoan(clientId, loanId) async {
-  final docRef = db.collection("clients").doc(clientId).collection("loans").doc(loanId);
+  final docRef =
+      db.collection("clients").doc(clientId).collection("loans").doc(loanId);
 
   await docRef.get().then((DocumentSnapshot doc) {
     final data = doc.data() as Map<String, dynamic>;
@@ -137,6 +138,90 @@ Future<void> updateLoan(clientId, loanId, interest, mount) async {
 Future<void> deleteLoan(clientId, loanId) async {
   final docRef =
       db.collection("clients").doc(clientId).collection("loans").doc(loanId);
+
+  await docRef.delete().then(
+        (doc) => print("Document deleted"),
+        onError: (e) => print("Error updating document $e"),
+      );
+}
+
+// Trae todos los pagos
+Future<List> getAllPayments(clientId, loanId) async {
+  final docRef = db
+      .collection("clients")
+      .doc(clientId)
+      .collection("loans")
+      .doc(loanId)
+      .collection("payments");
+  final payments = [];
+
+  await docRef.get().then(
+    (querySnapshot) {
+      for (var docSnapshot in querySnapshot.docs) {
+        payments.add(docSnapshot.data());
+      }
+    },
+    onError: (e) => print("Error completing: $e"),
+  );
+  print(payments);
+  return payments;
+}
+
+// Agrega nuevo pago
+Future<void> addPayment(clientId, loanId, payment) async {
+  final clientRef = db
+      .collection("clients")
+      .doc(clientId)
+      .collection("loans")
+      .doc(loanId)
+      .collection("payments")
+      .withConverter(
+        fromFirestore: Loan.fromFirestore,
+        toFirestore: (Loan loan, options) => loan.toFirestore(),
+      );
+
+  await clientRef.add(payment).then((documentSnapshot) =>
+      print("Added Data with ID: ${documentSnapshot.id}"));
+}
+
+// Trae datos de un pago
+Future<String> getPayment(clientId, loanId, paymentId) async {
+  final docRef = db
+      .collection("clients")
+      .doc(clientId)
+      .collection("loans")
+      .doc(loanId)
+      .collection("payments")
+      .doc(paymentId);
+
+  await docRef.get().then((DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    print(data);
+    return data;
+  }, onError: (e) => print("error getting loan document: $e"));
+
+  return "";
+}
+
+// Actualiza un pago
+Future<void> updatePayment(clientId, loanId, mount, date) async {
+  final docRef =
+      db.collection("clients").doc(clientId).collection("loans").doc(loanId);
+
+  await docRef.update({"mount": mount, "date": date}).then(
+      (value) => print("DocumentSnapshot successfully updated!"),
+      onError: (e) => print("Error updating document $e"));
+}
+
+// Elimina un pago
+Future<void> deletePayment(clientId, loanId, paymentId) async {
+  final docRef = db
+      .collection("clients")
+      .doc(clientId)
+      .collection("loans")
+      .doc(loanId)
+      .collection("payments")
+      .doc(paymentId);
 
   await docRef.delete().then(
         (doc) => print("Document deleted"),
